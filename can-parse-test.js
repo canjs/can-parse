3f231ec3d22413ee5d81916d7d9fe97146ed992a
Justin Meyer <justinbmeyer@gmail.com>
2017-01-18 22:22:17 -0600

    more progress

--- can-parse-test.js
+++ can-parse-test.js
@@ -16,11 +16,14 @@ var grammar = {
 		"SPACE": /^\s/,
 		"'": /^'/,
 		'"': /^"/,
+		"=": /^=/,
 		ALPHA_NUMERIC: /^[A-Za-z0-9]+/,
 		TAGNAME: /^[a-z][-:_A-Za-z0-9]*/,
 		NOT_END_MAGIC: /^([^\}]*)/,
 		NOT_SPACE: /^[^\s\{\}<]+/,
-		NOT_SPACE_RIGHT_CARROT: /^[^\s>]+/
+		NOT_SPACE_RIGHT_CARROT: /^[^\s>=\{]+/,
+		NOT_MAGIC_OR_DOUBLE: /^[^"\{]+/,
+		NOT_MAGIC_OR_SINGLE: /^[^'\{]+/
 	},
 	tree: {
 		EXPRESSION: [
@@ -44,25 +47,37 @@ var grammar = {
 		],
 		ATTRS: [
 			["ATTR"],
+			["ATTR", "SPACE", "ATTRS"],
 			["MAGIC"],
-			["ATTRS", "SPACE", "ATTR"],
-			["ATTRS", "SPACE", "MAGIC"]
+			["MAGIC","ATTRS"]
 		],
 		ATTR: [
 			["QUOTE","=","QUOTE"],
 			["NOT_SPACE_RIGHT_CARROT","=","QUOTE"],
-			["NOT_SPACE_RIGHT_CARROT","=","alpha"],
+			["NOT_SPACE_RIGHT_CARROT","=","NOT_SPACE_RIGHT_CARROT"],
 			["NOT_SPACE_RIGHT_CARROT","=","MAGIC"],
 			["NOT_SPACE_RIGHT_CARROT"]
 		],
 		QUOTE: [
-			["'","MAGIC_OR_TEXT","'"],
-			['"',"MAGIC_OR_TEXT",'"']
+			["'","SINGLE_QUOTE_MAGIC_OR_TEXT","'"],
+			['"',"DOUBLE_QUOTE_MAGIC_OR_TEXT",'"']
+		],
+		SINGLE_QUOTE_MAGIC_OR_TEXT: [
+			["NOT_MAGIC_OR_SINGLE"],
+			["NOT_MAGIC_OR_SINGLE","SINGLE_QUOTE_MAGIC_OR_TEXT" ],
+			["MAGIC"],
+			["MAGIC", "SINGLE_QUOTE_MAGIC_OR_TEXT"]
+		],
+		DOUBLE_QUOTE_MAGIC_OR_TEXT: [
+			["NOT_MAGIC_OR_DOUBLE"],
+			["NOT_MAGIC_OR_DOUBLE","DOUBLE_QUOTE_MAGIC_OR_TEXT" ],
+			["MAGIC"],
+			["MAGIC", "DOUBLE_QUOTE_MAGIC_OR_TEXT"]
 		],
 		MAGIC_OR_TEXT: [
 			["TEXT"],
-			["MAGIC"],
 			["TEXT","MAGIC_OR_TEXT" ],
+			["MAGIC"],
 			["MAGIC", "MAGIC_OR_TEXT"]
 		],
 		MAGIC: [
@@ -70,25 +85,75 @@ var grammar = {
 		],
 		TEXT: [
 			["SPACE"],
-			["SPACE", "TEXT"]
+			["SPACE", "TEXT"],
 			["NOT_SPACE"],
-			["NOT_SPACE", "TEXT"],
-
+			["NOT_SPACE", "TEXT"]
 		]
 	}
 };
 
+/*
 QUnit.test('parse.getExpressionLex', function(){
 
 	var expressionLex = parse.getExpressionStartLex(grammar);
 
 	console.log(expressionLex);
 
-});
+});*/
 
-QUnit.test("parse basics", function(){
+QUnit.test("parse.getLexPossibilities", function(){
 	var parser = parse(grammar);
 
-	var out = parser("Hi{world}!");
-	console.log(out);
+	var lexPossibilities = parser.getLexPossibilities("TAG",[2,3,4,5], 3);
+	console.log(lexPossibilities);
+	QUnit.deepEqual(lexPossibilities, {
+		">": [
+			["TAG", [2]]
+		],
+		"/>": [
+			["TAG", [3]]
+		],
+		"'": [
+			["TAG", [4,5]],
+			["ATTRS", [0,1]],
+			["ATTR", [0]],
+			["QUOTE", [0]],
+		],
+		'"': [
+			["TAG", [4,5]],
+			["ATTRS", [0,1]],
+			["ATTR", [0]],
+			["QUOTE", [1]]
+		],
+		NOT_SPACE_RIGHT_CARROT: [
+			["TAG", [4,5]],
+			["ATTRS", [0,1]],
+			["ATTR", [1,2,3,4]],
+		],
+		"{": [
+			["TAG", [4,5]],
+			["ATTRS", [2,3]],
+			["MAGIC", [0]]
+		]
+	});
+
+});
+/*
+QUnit.test("COMPLETED", function(){
+	var parser = parse(grammar);
+
+	QUnit.deepEqual(parser.getLexPossibilities("ATTR",[1,2,3,4], 1),{
+		"=": [
+			["ATTR", [1,2,3]]
+		],
+		EXPRESSION_COMPLETED: [
+			["ATTR", [4]]
+		]
+	});
+});*/
+
+QUnit.test("basics", function(){
+	var parser = parse(grammar);
+	parser("<my-element bar='car'/>");
+
 });
--- can-parse.js
+++ can-parse.js
@@ -1,5 +1,6 @@
 var each = require("can-util/js/each/each");
 var assign = require("can-util/js/assign/assign");
+var last = require("can-util/js/last/last");
 /*
  *
  */
@@ -12,19 +13,61 @@ var eachBreak = function(obj, callback){
 		}
 	}
 };
-
+var eachBackwardsBreak = function(arr,callback){
+	for(var len = arr.length, i = len - 1; i >= 0; i--) {
+		var res = callback(arr[i], i);
+		if(res !== undefined) {
+			return res;
+		}
+	}
+};
 
 
 
 // want to know available lex options at all times ...
 
 function makeParser(grammar){
-	var startLexTree = getExpressionStartLex(grammar);
+	var recurseCount = 0;
 
-	return function(text, callbacks) {
+	var parser = function(text, callbacks) {
+		recurseCount = 0;
+
+		var indexStack = [["EXPRESSION",1]];
+		var lexPossibilities = parser.getLexPossibilities("EXPRESSION", Object.keys(grammar.tree.EXPRESSION), 0);
+
+		var lexMatch = parser.getLexMatch(text, lexPossibilities);
+		parser.updateStack(indexStack, lexMatch);
+		while(lexMatch) {
+			recurseCount++;
+			if(recurseCount >= 500) {
+				return;
+			}
+
+			text = text.substr(lexMatch.match.length);
+			// update the stack
+
+			// [EXPRESSION, TAG]
+
+			//
+			// rules for processing the next value
+			var rules = lexMatch.rules;
+			var newLexMatch = eachBackwardsBreak(lexMatch.rules, function(rule){
+				// how does this become 1?
+				var subRulePossibilities = parser.getLexPossibilities(
+					rule[0],
+					rule[1],
+					parser.startingIndex(indexStack, rule));
+
+				return parser.getLexMatch(text, subRulePossibilities);
+			});
+			parser.updateStack(indexStack, newLexMatch);
+			lexMatch = newLexMatch;
+
+		}
 
 
-		var lexPossibilities = getLexPossibilities("EXPRESSION", "*")
+		return;
+		var lexPossibilities = parser.getLexPossibilities("EXPRESSION", "*");
 
 		lexPossibilities = {
 			"<": {
@@ -58,25 +101,25 @@ function makeParser(grammar){
 		var rule = {
 			EXPRESSION: [0,3],
 			TAG: [0,1,2,3,4,5]
-		}
+		};
 
 		// now we move the text
-		var text = "my-element bar='car'/>";
+		text = "my-element bar='car'/>";
 		// we want to check the "next" step in magic first ...
 
-		var lexPossibilities = getLexPossibilities("TAG",[0,1,2,3,4,5], 1);
+		lexPossibilities = parser.getLexPossibilities("TAG",[0,1,2,3,4,5], 1);
 		lexPossibilities = {
 			TAGNAME: {
 				TAG: [0,1,2,3,4,5]
 			}
 		}; // might need some way of saying it MUST be a tag here or previous lex
-		matchedLex = "TAGNAME"
+		matchedLex = "TAGNAME";
 
 		text = " bar='car'/>";
 		rule = {
 			TAG: [0,1,2,3,4,5]
 		};
-		lexPossibilities = getLexPossibilities("TAG",[0,1,2,3,4,5], 2)
+		lexPossibilities = parser.getLexPossibilities("TAG",[0,1,2,3,4,5], 2);
 		lexPossibilities = {
 			">": {
 				TAG: [0]
@@ -93,8 +136,8 @@ function makeParser(grammar){
 		text = "bar='car'/>";
 		rule = {
 			TAG: [2,3,4,5]
-		}
-		lexPossibilities = getLexPossibilities("TAG",[2,3,4,5], 3)
+		};
+		lexPossibilities = parser.getLexPossibilities("TAG",[2,3,4,5], 3);
 		lexPossibilities = {
 			">": {
 				TAG: [2]
@@ -128,109 +171,112 @@ function makeParser(grammar){
 		matchedLex = "NOT_SPACE_RIGHT_CARROT";
 
 		text = "='car'/>";
-		matchedRule = {
+		rule = {
 			TAG: [4,5],
 			ATTRS: [0,1],
 			ATTR: [1,2,3,4],
 		};
-		lexPossibilities = getLexPossibilities("ATTR",[1,2,3,4], 1);
+		lexPossibilities = parser.getLexPossibilities("ATTR",[1,2,3,4], 1);
 		lexPossibilities = {
 			"=": {
 				ATTR: [1,2,3]
 			},
 			BREAK: [4]
-		}
+		};
 		lexPossibilities = "=";
 
 		return ruleMatch;
 	};
 
-}
 
-// lowest next has first priority
-var res = {
-	EXPRESSION: {
-		"*": "anything"
-	},
-	MAGIC_OR_TEXT: [
-		"{", "MAGIC"
-	],
-	TEXT: [
-		"SPACE","NOT_SPACE"
-	]
-};
-// provide the possibilities for a given position
-function getLexPossibilities(expression, rule, position, startLexTree, grammar){
-	startLexTree[expression]
-}
 
-function getRulesMatch(text, startLex, grammar) {
-	return eachBreak(startLex, function(rules, lexKey){
-		var result = grammar.lex[lexKey].exec(text);
-		if(result) {
-			return {
-				match: result[0],
-				rules: rules,
-				lex: lexKey
-			};
+	parser.getLexPossibilities = function(expression, rules, INDEX){
+		recurseCount++;
+		if(recurseCount > 500) {
+			console.warn("too much recursion");
+			return;
 		}
 
-	});
-}
-
-
-var addRulesForExpression, addExpression, recurseCount = 0;;
-
-addExpression = function(rule, startLexes, grammar) {
-	recurseCount++;
-	if(recurseCount > 500) {
-		console.warn("too much recursion");
-		return;
-	}
-	var firstExpr = rule[0];
-	if(startLexes[firstExpr]) {
-		return startLexes[firstExpr];
-	}
-	var lexes = {};
-
-	if(grammar.lex[firstExpr]) {
-		lexes[firstExpr] = true;
-	} else if(grammar.tree[firstExpr]){
-		assign(lexes, addRulesForExpression(firstExpr, startLexes, grammar) );
-	} else {
-		console.warn("there is no ", firstExpr, "in the grammar tree");
-	}
-	return lexes;
-};
-
-addRulesForExpression = function(expression, startLexes, grammar) {
-	recurseCount++;
-	if(recurseCount > 500) {
-		console.warn("too much recursion");
-		return;
-	}
-
-	startLexes[expression] = {};
-	var expressionLexes = {};
-	grammar.tree[expression].forEach(function(rule, index){
-		var lexes = addExpression(rule, startLexes, grammar);
-		each(lexes, function(v, lex){
-			if( !startLexes[expression][lex] ) {
-				startLexes[expression][lex] = [];
+		var expressionLexes = {};
+		// map of which rules were at a given sub-expression
+		var subExpressionRules = {}; //{ATTRS: [4,5]}
+		rules.forEach(function(index){
+			index = +index;
+			// we are going to get ATTRS
+			var rule = grammar.tree[expression][index][INDEX];
+			if(!rule) {
+				// this might be a "EXPRESSION_COMPLETED" rule ...
+				if(grammar.tree[expression][index].length === INDEX) {
+					rule = "EXPRESSION_COMPLETED";
+				} else {
+					throw new Error("Expression "+expression+", Rule: "+index+" has no futher tokens");
+				}
 			}
-			startLexes[expression][lex].push(index);
-		});
-		assign(expressionLexes, lexes);
-	});
-	return expressionLexes;
-};
+			if(!subExpressionRules[rule]) {
+				subExpressionRules[rule] = [];
+			}
+			subExpressionRules[rule].push(index);
+			// returns the available lexes ... we need to add these to expression lexes
+			// probably make sure there's no conflict.
 
-function getExpressionStartLex(grammar) {
-	var startLexes = {};
-	addRulesForExpression("EXPRESSION", startLexes, grammar);
-	return startLexes;
+			// we only need to get this once
+			if(grammar.lex[rule] || rule === "EXPRESSION_COMPLETED") {
+				expressionLexes[rule] = [[expression, subExpressionRules[rule]]];
+			}
+			else if( subExpressionRules[rule].length === 1 ) {
+				var lexes = parser.getLexPossibilities(rule,Object.keys(grammar.tree[rule]), 0);
+
+				each(lexes, function(subExpressionAndRules, lex){
+					subExpressionAndRules = subExpressionAndRules.slice(0);
+					subExpressionAndRules.unshift([expression, subExpressionRules[rule] ]);
+					expressionLexes[lex] = subExpressionAndRules;
+				});
+			}
+		});
+
+		return expressionLexes;
+	};
+
+	parser.getLexMatch = function(text, lexPossibilities) {
+		return eachBreak(lexPossibilities, function(rules, lexKey){
+			var result = grammar.lex[lexKey].exec(text);
+			if(result) {
+				return {
+					match: result[0],
+					rules: rules,
+					lex: lexKey
+				};
+			}
+
+		});
+	};
+	parser.updateStack = function(stack, lexMatch) {
+		var topOfStack = last(stack);
+		var lastRule = last(lexMatch.rules);
+		if(lastRule[0] !== topOfStack[0]) {
+			stack.push([lastRule[0],0]);
+		} else {
+			topOfStack[1]++;
+		}
+	};
+	parser.ruleSameAsCurrent = function(indexStack, rule) {
+		var topOfStack = last(indexStack);
+		return topOfStack[0] === rule[0];
+	};
+	parser.startingIndex = function(indexStack, rule) {
+		if(parser.ruleSameAsCurrent(indexStack, rule)) {
+			return last(indexStack)[1]+1;
+		} else {
+			return 1;
+		}
+	};
+
+	return parser;
+
 }
 
-makeParser.getExpressionStartLex = getExpressionStartLex;
+
+
+
 
 module.exports = makeParser;
