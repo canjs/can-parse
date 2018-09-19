6ec8de4940eaeb4568582c871d6f1e49d246929c
Justin Meyer <justinbmeyer@gmail.com>
2018-09-19 11:06:12 -0500

    some more baloney

--- ast-test.js
+++ ast-test.js
@@ -5,95 +5,49 @@ QUnit.test("basics", function(){
 	var ast = {
 		type: "EXPRESSIONS",
 		body: [
-			type: "EXPRESSION":
-		]
-	}
+			{
+				type: "EXPRESSION",
+				body: [
+					{
+						type:"TAG",
+						body: [
+							{lex: "<", match: "<", index: 0},
+							{lex: "TAGNAME", match: "my-element", index: 1 },
+							{lex: "SPACE", match: " ", index: 11 },
+							{
+								type: "ATTRS",
+								body: [
+									{
+										type: "ATTR",
+										body: [
+											{ lex: "NOT_SPACE_RIGHT_CARROT", "match": "bar", index: 12 },
+											{ lex: "=", "match": "=", index: 15},
+											{
+												type: "QUOTE",
+												body: [
+													{ lex: "'", "match": "'", index: 16 },
+													{
+														type: "SINGLE_QUOTE_MAGIC_OR_TEXT",
+														body: [
+															{ "lex": "NOT_MAGIC_OR_SINGLE", "match": "car", index: 17 }
+														]
+													},
+													{ "lex": "'", "match": "'", index: 20 }
+												]
+											}
+										]
+									}
+								]
+							},
+							{ "lex": "/>", "match": "/>", index: 21 }
+						]
 
-	var calls = [
-		[
-			{ "lex": "<", "match": "<", "index": 0 },
-			{
-				"start": [
-					{
-						"expression": "EXPRESSION",
-						"ruleIndexes": [ 0, 3 ]
-					},
-					{
-						"expression": "TAG",
-						"ruleIndexes": [ 0, 1, 2, 3, 4, 5]
+
 					}
-				],
-				end: []
-			}
-		],
-		[
-			{ "lex": "TAGNAME", "match": "my-element", index: 1 },
-			{ "end": [], "start": [] }
-		],
-		[
-			{ "lex": "SPACE", "match": " ", index: 11 },
-			{ "end": [], "start": [] }
-		],
-		[
-			{ "lex": "NOT_SPACE_RIGHT_CARROT", "match": "bar", index: 12 },
-			{
-				"end": [],
-				"start": [
-					{ "expression": "ATTRS", "ruleIndexes": [ 0, 1 ] },
-					{ "expression": "ATTR", "ruleIndexes": [ 1, 2, 3, 4 ] }
 				]
-			}
-		],
-		[
-			{ "lex": "=", "match": "=", index: 15},
-			{ "end": [], "start": [] }
-		],
-		[
-			{ "lex": "'", "match": "'", index: 16 },
-			{
-				"end": [],
-				"start": [
-					{ "expression": "QUOTE", "ruleIndexes": [ 0 ] }
-				]
-			}
-		],
-		[
-			{ "lex": "NOT_MAGIC_OR_SINGLE", "match": "car", index: 17 },
-			{
-				"end": [],
-				"start": [
-					{ "expression": "SINGLE_QUOTE_MAGIC_OR_TEXT", "ruleIndexes": [ 0, 1 ] }
-				]
-			}
-		],
-		[
-			{ "lex": "'", "match": "'", index: 20 },
-			{
-				"end": [
-					{ "expression": "SINGLE_QUOTE_MAGIC_OR_TEXT", "ruleIndexes": [ 0 ], tokenIndex: 0 },
-					{ "expression": "QUOTE", "ruleIndexes": [ 0 ], tokenIndex: 2 }
-				],
-				"start": []
-			}
-		],
-		[
-			{ "lex": "/>", "match": "/>", index: 21 },
-			{
-				"end": [
-					{ "expression": "ATTR", "ruleIndexes": [ 1 ], tokenIndex: 2 },
-					{ "expression": "ATTRS", "ruleIndexes": [ 0 ], tokenIndex: 0, },
-					{ "expression": "TAG", "ruleIndexes": [ 5 ], tokenIndex: 4 }
-				],
-				"start": []
-			}
-		],
-		[
-			{ "lex": null, "token": null, "match": null, "index": 23 },
-			{
-				"end": [
-					{ "expression": "EXPRESSION", "ruleIndexes": [ 0 ], tokenIndex: 0 }
-				]
-			}
+			},
+			{ "lex": null, "token": null, "match": null, "index": 23 }
 		]
-	];
+	};
+
 })
